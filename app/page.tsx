2c856007af87d65620dd8942c390666b4ddb8635
Rizky Rahman Salam <clashikik@gmail.com>
2024-08-05 06:21:07 +0700

    feat: change readme, footer

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <footer className="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
             <p>
               Layout & Design by
-              <a href="https://brittanychiang.com/" className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer"> Brittany Chiang</a>.
+              <a href="https://brittanychiang.com/" className="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300" target="_blank" rel="noreferrer"> Rizky Rahman Salam</a>.
             </p>
           </footer>
         </div>
